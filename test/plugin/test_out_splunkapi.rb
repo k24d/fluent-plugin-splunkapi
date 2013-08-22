c29387400ea2eb437010b5bbf6ad9ace08610449
NANKI Haruo <nanki@dotswitch.net>
2013-08-22 15:38:19 +0900

    add tests.

--- test/plugin/test_out_splunkapi.rb
+++ test/plugin/test_out_splunkapi.rb
@@ -3,6 +3,7 @@ require 'helper'
 class SplunkAPIOutputTest < Test::Unit::TestCase
   def setup
     Fluent::Test.setup
+    @time = Time.parse("2010-01-02 13:14:15 UTC").to_i
   end
 
   CONFIG = %[
@@ -18,19 +19,42 @@ class SplunkAPIOutputTest < Test::Unit::TestCase
 
   def test_configure
     # default
-    d = create_driver
+    d = create_driver CONFIG
     assert_equal 'rest', d.instance.protocol
     assert_equal '{TAG}', d.instance.source
     assert_equal 'fluent', d.instance.sourcetype
   end
 
-  def test_write
-    d = create_driver
+  def test_write_json
+    d = create_driver [CONFIG, "format json", "time_format unixtime"].join "\n"
 
-    time = Time.parse("2010-01-02 13:14:15 UTC").to_i
-    d.emit({"a"=>1}, time)
-    d.emit({"a"=>2}, time)
+    d.emit({"a"=>1}, @time)
+    d.emit({"a"=>2}, @time)
 
-    d.run
+    result = d.run
+    assert_equal 2, result['test'].size
+    assert_equal "#{@time}: {\"a\":1}", result['test'][0].strip
+  end
+
+  def test_write_kvp
+    d = create_driver [CONFIG, "format kvp", "time_format unixtime"].join "\n"
+
+    d.emit({"a"=>1}, @time)
+    d.emit({"a"=>2}, @time)
+
+    result = d.run
+    assert_equal 2, result['test'].size
+    assert_equal "#{@time}: a=\"1\"", result['test'][0].strip
+  end
+
+  def test_write_kvp_without_time
+    d = create_driver [CONFIG, "format kvp", "time_format none"].join "\n"
+
+    d.emit({"a"=>1}, @time)
+    d.emit({"a"=>2}, @time)
+
+    result = d.run
+    assert_equal 2, result['test'].size
+    assert_equal "a=\"1\"", result['test'][0].strip
   end
 end
